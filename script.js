4eba1df3ae3cb73f5d36e722366b7dd9c052f739
Jones <ebjones@c4b301cf6e21.ant.amazon.com>
2017-01-14 21:09:48 -0800

    added feature to accept query string parameters to set timer

--- script.js
+++ script.js
@@ -1,14 +1,51 @@
-var loader = document.getElementById('loader'),
-  alpha = 0,
-  pi = Math.PI,
-  totalMinutes = 0;
+//global variables
+var alpha = 0,
+ pi = Math.PI;
+ totalMinutes = 0;
+ loader = document.getElementById('loader');
+
+function getParameterByName(name, url) {
+    if (!url) {
+      url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+purple = getParameterByName('purple');
+green = getParameterByName('green');
+yellow = getParameterByName('yellow');
+
+
+
+document.getElementById('timeset').addEventListener("load", queryString());
+
+function queryString() {
+  console.log('starting querystring');
+      if (purple||green||yellow){
+        console.log('parameters found in url.');
+        console.log('purple: ',purple);
+        console.log('green: ',green);
+        console.log('yellow: ',yellow)
+        setTime();
+      }
+      else {
+        document.getElementById('timeset').style.display = 'block';
+        console.log('no parameters in url. continuing.');
+      }
+    };
+
+//var loader = document.getElementById('loader');
 
 function setTime() {
-  document.getElementById('set').disabled = true;
-
-  preTarget = document.getElementById('setPreTarget').value;
-  target = document.getElementById('setTarget').value;
-  postTarget = document.getElementById('setPostTarget').value;
+document.getElementById('timeset').style.display = 'none';
+  preTarget = purple;
+  target = green;
+  postTarget = yellow;
   console.log('preTarget: ' + preTarget, '  target: ' + target, '  postTarget: ' + postTarget);
   
   totalMinutes = (+preTarget) + (+target) + (+postTarget);
@@ -41,8 +78,10 @@ function setTime() {
 
 
 function draw(path,startValue,stopValue) {
+  console.log('drawing...');
   path = document.getElementById(path);
   console.log('path:',path);
+  console.log('start: ' + startValue +' stop: '+ stopValue);
   alpha1 = startValue;
   alpha2 = stopValue;
   var r1 = (alpha1 * pi / 180),
@@ -59,10 +98,11 @@ function draw(path,startValue,stopValue) {
 
 
 function timer() {
+  console.log('starting timer...');
   var t = ((totalMinutes * 1000) / 6); /*divide by 6 to get minutes because 360/60 = 6*/
   alpha++;
 
-  console.log("minutes", totalMinutes.value, "t", t, "alpha", alpha);
+  console.log("minutes", totalMinutes, "t", t, "alpha", alpha);
 
   var r = (alpha * pi / 180),
     x = Math.sin(r) * 125,
@@ -84,12 +124,18 @@ function timer() {
 
 /* Set Colored Sections and Start Timer */
 document.getElementById('set').onclick = function(){
+  purple = document.getElementById('setPreTarget').value;
+  green = document.getElementById('setTarget').value;
+  yellow = document.getElementById('setPostTarget').value;
   setTime();
 };
 
 document.getElementById('setPostTarget').addEventListener('keyup', function(event) {
   event.preventDefault();
   if (event.keyCode == 13) {
+    purple = document.getElementById('setPreTarget').value;
+    green = document.getElementById('setTarget').value;
+    yellow = document.getElementById('setPostTarget').value;
     setTime();
   }
 });
@@ -97,6 +143,9 @@ document.getElementById('setPostTarget').addEventListener('keyup', function(even
 document.getElementById('set').addEventListener('keyup', function(event) {
   event.preventDefault();
   if (event.keyCode == 13) {
+    purple = document.getElementById('setPreTarget').value;
+    green = document.getElementById('setTarget').value;
+    yellow = document.getElementById('setPostTarget').value;
     setTime();
   }
 });
